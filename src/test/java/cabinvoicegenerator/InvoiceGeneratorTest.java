fb0b9bc35dd3851843b7cfee16744a6e807b3918
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-24 10:04:35 +0530

    [Surajmal kumar].Comparing Fare.returning Minimum fair if it is greater than totalFare else returning totalFare.

--- src/test/java/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -7,7 +7,7 @@ import org.junit.Assert;
 
 
 
-public class AppTest 
+public class InvoiceGeneratorTest 
 {
     
     @Test
@@ -20,4 +20,13 @@ public class AppTest
      
           Assert.assertEquals(25, fare,0.0);   
     }
+    @Test
+	public void givenLessDistanceOrTime_ShouldReturnTotalMiNFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
+	}
+    
 }
