a028da793c138a9dd7794b75af9e6345b086e064
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-10-24 09:59:58 +0530

    [abhishek] add: return minimum fare if distance or time is less

--- src/test/java/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -14,4 +14,13 @@ public class InvoiceGeneratorTest {
 
 		Assert.assertEquals(25, fare, 0.0);
 	}
+
+	@Test
+	public void givenLessDistanceOrTime_ShouldReturnTotalMiNFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
+	}
 }
