a88841339de08c90d6ad8d733906ce3e4ebe1cc4
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-10-28 12:17:46 +0530

    [abhishek] add: given a user id the invoice service gets the list of rides

--- src/main/java/cabinvoicegenerator/InvoiceService.java
+++ src/main/java/cabinvoicegenerator/InvoiceService.java
@@ -4,12 +4,14 @@ public class InvoiceService {
 	private static final int COST_PER_TIME = 1;
 	private static final double MINIMUM_COST_PER_KILOMETER = 10;
 	private static final double MINIMUM_FARE = 5;
-
+    private RideRepository rideRepository=null;
 	public double calculateFare(double distance, int time) {
 		double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
 		return Math.max(totalFare, MINIMUM_FARE);
 	}
-	
+	public InvoiceService() {
+		this.rideRepository=new RideRepository();
+	}
 	public double calculateFare1(Ride[] rides) {
 		double totalFare = 0;
 		for (Ride ride : rides) {
@@ -29,12 +31,12 @@ public class InvoiceService {
 	}
 
 	public void addRides(String userId, Ride[] rides) {
-		
+	     rideRepository.addRides(userId,rides);	
 	}
 
 	public InvoiceSummary getInvoiceSummary(String userId) {
 		// TODO Auto-generated method stub
-		return null;
+		return this.calculateFare(rideRepository.getRides(userId));
 	}
 	
 }
---
+++ src/main/java/cabinvoicegenerator/RideRepository.java
@@ -0,0 +1,25 @@
+package cabinvoicegenerator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RideRepository {
+	Map<String, ArrayList<Ride>> userRides = null;
+
+	public RideRepository() {
+		this.userRides = new HashMap<>();
+	}
+
+	public void addRides(String userId, Ride[] rides) {
+		ArrayList<Ride> rideList = userRides.get(userId);
+		if (rideList == null) {
+			this.userRides.put(userId, new ArrayList<>(Arrays.asList(rides)));
+		}
+	}
+
+	public Ride[] getRides(String userId) {
+		return  this.userRides.get(userId).toArray(new Ride[0]);
+	}
+}
